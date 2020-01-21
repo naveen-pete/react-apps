0d49ad439bd1a9267fbd081ec336cfba6408f844
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-01-21 13:52:16 +0530

    commit pagination app into the repo

--- blogger-app/src/components/App.js
+++ blogger-app/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Header from './Header';
@@ -7,18 +7,28 @@ import Posts from './Posts';
 import PostFormCreate from './PostFormCreate';
 import PostFormUpdate from './PostFormUpdate';
 import PostDetail from './PostDetail';
+import { getCategories } from '../api/categories';
 
-const App = () => (
-  <div className="container">
-    <Header />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/posts" component={Posts} />
-      <Route path="/posts/new" component={PostFormCreate} />
-      <Route path="/posts/:id/edit" component={PostFormUpdate} />
-      <Route path="/posts/:id" component={PostDetail} />
-    </Switch>
-  </div>
-);
+const App = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const categories = getCategories();
+    setCategories(categories);
+  }, []);
+
+  return (
+    <div className="container">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/posts" component={Posts} />
+        <Route path="/posts/new" component={PostFormCreate} />
+        <Route path="/posts/:id/edit" component={PostFormUpdate} />
+        <Route path="/posts/:id" component={PostDetail} />
+      </Switch>
+    </div>
+  );
+}
 
 export default App;
